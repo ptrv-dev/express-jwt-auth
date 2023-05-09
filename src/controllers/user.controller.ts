06c6daf5846df228fd51fbe973dbcf0cae6f7f27
ptrv-dev <petrpv99119@gmail.com>
2023-05-09 12:08:51 +0300

    User Controller handle custom errors

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,30 +1,30 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import * as UserService from '../services/user.service';
 
-export const registration = async (req: Request, res: Response) => {
+export const registration = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { username, email, password } = req.body;
     const response = await UserService.registration(username, email, password);
     return res.status(200).json(response);
   } catch (error) {
-    if (!(error instanceof Error)) return;
-    console.error('Error: User registration error!\n', error);
-    return res
-      .status(500)
-      .json({ error: 'User registration error', message: error.message });
+    next(error);
   }
 };
 
-export const login = async (req: Request, res: Response) => {
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { email, password } = req.body;
     const response = await UserService.login(email, password);
     return res.status(200).json(response);
   } catch (error) {
-    if (!(error instanceof Error)) return;
-    console.error('Error: User login error!\n', error);
-    return res
-      .status(500)
-      .json({ error: 'User login error', message: error.message });
+    next(error);
   }
 };
