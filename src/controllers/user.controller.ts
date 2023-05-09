ff1fd452677a0385c06483115883d7c598665c0a
ptrv-dev <petrpv99119@gmail.com>
2023-05-09 11:13:39 +0300

    User Controller - Login

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -14,3 +14,17 @@ export const registration = async (req: Request, res: Response) => {
       .json({ error: 'User registration error', message: error.message });
   }
 };
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const response = await UserService.login(email, password);
+    return res.status(200).json(response);
+  } catch (error) {
+    if (!(error instanceof Error)) return;
+    console.error('Error: User login error!\n', error);
+    return res
+      .status(500)
+      .json({ error: 'User login error', message: error.message });
+  }
+};
