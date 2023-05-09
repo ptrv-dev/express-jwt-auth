d4fde3f1a750a5ec72095e71c8856bed29509e2d
ptrv-dev <petrpv99119@gmail.com>
2023-05-09 14:13:23 +0300

    Router added auth get me endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import { Express, Router } from 'express';
 const router = Router();
 
 import * as UserController from '../controllers/user.controller';
+import checkAuthMiddleware from '../middlewares/check-auth.middleware';
 
 router.post('/auth/registration', UserController.registration);
 router.post('/auth/login', UserController.login);
 router.get('/auth/refresh', UserController.refresh);
+router.get('/auth/get', checkAuthMiddleware, UserController.getMe);
 
 export default router;
