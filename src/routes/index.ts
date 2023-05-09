ddfff4f84eae7afb041ea7781cef50289177fdd8
ptrv-dev <petrpv99119@gmail.com>
2023-05-09 13:33:54 +0300

    Add auth refresh endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,5 +5,6 @@ import * as UserController from '../controllers/user.controller';
 
 router.post('/auth/registration', UserController.registration);
 router.post('/auth/login', UserController.login);
+router.get('/auth/refresh', UserController.refresh);
 
 export default router;
